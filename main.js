518bc7cdde6027f09be13aa3c3039ddf08e72338
Juan Dager <68037638+pekingPow@users.noreply.github.com>
2024-01-26 18:16:25 -0500

    add speed and friction

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
@@ -9,6 +8,8 @@ animate();
 
 function animate() {
   car.update();
+  canvas.height = window.innerHeight;
+
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
