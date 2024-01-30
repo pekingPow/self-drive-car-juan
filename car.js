e1f6f650bc5b87d634550a238857859b0ea86cb0
Juan Dager <68037638+pekingPow@users.noreply.github.com>
2024-01-29 21:34:19 -0500

    added yellow sensors

--- car.js
+++ car.js
@@ -11,10 +11,12 @@ class Car {
     this.maxSpeed = 3;
     this.friction = 0.05;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
   update() {
     this.#move();
+    this.sensor.update();
   }
   #move() {
     if (this.controls.forward) {
@@ -59,5 +61,7 @@ class Car {
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
